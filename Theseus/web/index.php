6061a1e9556e9cec62b8d5985e7c871a7d303b0b
MiKzZ91 <m.longpres@gmail.com>
2015-01-30 14:34:25 +0100

    bande de pd

--- Theseus/web/index.php
+++ Theseus/web/index.php
@@ -9,20 +9,4 @@
 require '../../vendor/autoload.php';
 require_once "config/config.php";
 
-use model\Client;
-use control\Control;
-
-$array = ["nom"=>"sususususuus",
-    "prenom"=>"sususususuus",
-    "dateNaissance"=>"10/10/2014",
-    "adresse"=>"sususususuus",
-    "cp" => 2,
-];
-
-$client = new client($array);
-$Control = new Control($bdd);
-
-$test = $Control->add($client);
-
-var_dump($test);
 
