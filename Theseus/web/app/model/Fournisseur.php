746c053b7f01b4f96358350dc05255de78c264ec
heisenberg7 <f.aymardruby@gmail.com>
2015-01-30 14:54:38 +0100

    Création classes Produit et Fournisseur

---
+++ Theseus/web/app/model/Fournisseur.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 30/01/2015
+ * Time: 14:49
+ */
+
+namespace model;
+
+
+class Fournisseur {
+
+    private $id;
+    private $nom;
+    private $adresse;
+    private $ville;
+    private $cp;
+    private $tel;
+
+    function __construct($id, $nom, $adresse, $ville, $cp, $tel)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->adresse = $adresse;
+        $this->ville = $ville;
+        $this->cp = $cp;
+        $this->tel = $tel;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param mixed $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * @param mixed $ville
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * @param mixed $cp
+     */
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * @param mixed $tel
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+    }
+
+
+
+}
---
+++ Theseus/web/app/model/Produit.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 30/01/2015
+ * Time: 14:42
+ */
+
+namespace model;
+
+
+class Produit {
+
+    private $id;
+    private $marque;
+    private $type;
+    private $modele;
+    private $libelle;
+    private $description;
+    private $prix;
+    private $stock;
+
+    function __construct($id, $marque, $type, $modele, $libelle, $description, $prix, $stock)
+    {
+        $this->id = $id;
+        $this->marque = $marque;
+        $this->type = $type;
+        $this->modele = $modele;
+        $this->libelle = $libelle;
+        $this->description = $description;
+        $this->prix = $prix;
+        $this->stock = $stock;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * @param mixed $stock
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * @param mixed $prix
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param mixed $libelle
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModele()
+    {
+        return $this->modele;
+    }
+
+    /**
+     * @param mixed $modele
+     */
+    public function setModele($modele)
+    {
+        $this->modele = $modele;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+
+    /**
+     * @param mixed $marque
+     */
+    public function setMarque($marque)
+    {
+        $this->marque = $marque;
+    }
+
+
+
+
+
+}
