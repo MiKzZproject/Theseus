04948353757490fb397ca7b1b333e6135d774ca1
heisenberg7 <f.aymardruby@gmail.com>
2015-02-08 15:37:07 +0100

    Ajout events
    Ajout style 2.css
    Ajout d'images
    
    /!\ CSS pas fini /!\

--- Theseus/web/index.php
+++ Theseus/web/index.php
@@ -17,6 +17,7 @@ require_once "config/config.php";
   <meta charset="utf-8">
   <title>Theseus</title>
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style2.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
 
@@ -27,7 +28,30 @@ require_once "config/config.php";
 
   </header>
   <section>
-
+      <h2>Events</h2>
+      <div class="grid">
+        <figure class="effect-goliath">
+          <img src="img/products/Calendar.jpg" alt=prochain-evenement"/>
+          <figcaption>
+            <h2>Prochain évènement</h2>
+            <p><a href="#">View more</a></p>
+          </figcaption>
+        </figure>
+        <figure class="effect-goliath">
+          <img src="img/products/Nexus7WirelessKeyboardCase.jpg" alt="Nexus7WirelessKeyboardCase"/>
+          <figcaption>
+            <h2>Produits phares</h2>
+            <p><a href="#">View more</a></p>
+          </figcaption>
+        </figure>
+        <figure class="effect-goliath">
+          <img src="img/products/Newsletter.jpg" alt="Newsletter"/>
+          <figcaption>
+            <h2>News et alertes</h2>
+            <p><a href="#">View more</a></p>
+          </figcaption>
+        </figure>
+      </div>
   </section>
   <footer>
 
