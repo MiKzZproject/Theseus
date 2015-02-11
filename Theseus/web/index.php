49be6ad80d932a62b516fbeccadd1383d2b17be4
heisenberg7 <f.aymardruby@gmail.com>
2015-02-11 21:37:26 +0100

    Slider home (avec description + responsive)
    Code du slider sur index.php
    Css du slider
    Js du slider
    
    /!\ slider pas encore mis en place donc code commenté/!\

--- Theseus/web/index.php
+++ Theseus/web/index.php
@@ -27,6 +27,42 @@ require_once "config/config.php";
 
 
   </header>
+
+  <!-- essai slider Flo    http://slippry.com/examples/news/    -->
+
+  <!--
+  <section id="news-demo">
+    <article>
+      <div class="text-content">
+        <h2>Image 1 du slider - Bugatti</h2>
+        <p>Description de l'image 1 du slider.</p>
+        <a href="#" class="button-link read-more">read more</a>
+      </div>
+      <div class="image-content"><img src="img/slider/slide1.jpg" alt="slide1"></div>
+    </article>
+    <article>
+      <div class="text-content">
+        <h2>Image 2 du slider - Paris</h2>
+        <p>Description de l'image 2 du slider.</p>
+        <a href="#" class="button-link read-more">read more</a>
+      </div>
+      <div class="image-content"><img src="img/slider/slide2.jpg" alt="slide2"></div>
+    </article>
+    <article>
+      <div class="text-content">
+        <h2>Image 3 du slider - Ciel/Nuage</h2>
+        <p>Description de l'image 3 du slider.</p>
+        <a href="#" class="button-link read-more">read more</a>
+      </div>
+      <div class="image-content"><img src="img/slider/slide3.jpg" alt="slide3"></div>
+    </article>
+  </section>
+
+   -->
+
+  <!-- essai slider Flo -->
+
+
   <section>
       <h2>Events</h2>
       <div class="grid">
@@ -60,5 +96,6 @@ require_once "config/config.php";
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="js/script.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="js/scriptSlider.js"></script>
 </body>
 </html>
---
+++ Theseus/web/js/scriptSlider.js
@@ -0,0 +1,23 @@
+/**
+ * Created by User on 11/02/2015.
+ */
+jQuery('#news-demo').slippry({
+    // general elements & wrapper
+    slippryWrapper: '<div class="sy-box news-slider" />', // wrapper to wrap everything, including pager
+    elements: 'article', // elments cointaining slide content
+
+    // options
+    adaptiveHeight: false, // height of the sliders adapts to current
+    captions: false,
+
+    // pager
+    pagerClass: 'news-pager',
+
+    // transitions
+    transition: 'horizontal', // fade, horizontal, kenburns, false
+    speed: 1200,
+    pause: 8000,
+
+    // slideshow
+    autoDirection: 'prev'
+});
