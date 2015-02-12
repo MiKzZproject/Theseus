593e644620fcbe27d56c3a2dee2283adbb361475
heisenberg7 <f.aymardruby@gmail.com>
2015-02-12 10:12:22 +0100

    Ajout du slider home -> ok fini
    Slider home responsive et avec description
    - modif des dossiers images (others)
    - ajout des css, et js

--- Theseus/web/index.php
+++ Theseus/web/index.php
@@ -15,7 +15,9 @@ require_once "config/config.php";
 <html lang="fr">
 <head>
   <meta charset="utf-8">
+
   <title>Theseus</title>
+  <link rel="stylesheet" href="css/slippry.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/style2.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
@@ -27,10 +29,7 @@ require_once "config/config.php";
 
 
   </header>
-
-  <!-- essai slider Flo    http://slippry.com/examples/news/    -->
-
-  <!--
+  
   <section id="news-demo">
     <article>
       <div class="text-content">
@@ -58,30 +57,26 @@ require_once "config/config.php";
     </article>
   </section>
 
-   -->
-
-  <!-- essai slider Flo -->
-
 
   <section>
       <h2>Events</h2>
       <div class="grid">
         <figure class="effect-goliath">
-          <img src="img/products/testPaysage2.jpg" alt=paysage2"/>
+          <img src="img/others/testPaysage2.jpg" alt=paysage2"/>
           <figcaption>
             <h2>Prochain évènement</h2>
             <p><a href="#">En savoir +</a></p>
           </figcaption>
         </figure>
         <figure class="effect-goliath">
-          <img src="img/products/bulles.png" alt="bulles"/>
+          <img src="img/others/bulles.png" alt="bulles"/>
           <figcaption>
             <h2>Produits phares</h2>
             <p><a href="#">En savoir +</a></p>
           </figcaption>
         </figure>
         <figure class="effect-goliath">
-          <img src="img/products/testPaysage.jpg" alt="paysage"/>
+          <img src="img/others/testPaysage.jpg" alt="paysage"/>
           <figcaption>
             <h2>News et alertes</h2>
             <p><a href="#">En savoir +</a></p>
@@ -89,6 +84,7 @@ require_once "config/config.php";
         </figure>
       </div>
   </section>
+
   <footer>
 
   </footer>
@@ -96,6 +92,8 @@ require_once "config/config.php";
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="js/script.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="js/slippry.js"></script>
   <script src="js/scriptSlider.js"></script>
+
 </body>
 </html>
