ed6b2e21d9bc3f9c62727bd2f94667b721a06d28
quaresma95 <quaresma217@gmail.com>
2015-02-27 12:22:32 +0100

    Revert "Revert "ajout categorie model controller""
    This reverts commit 22fb82a70ebcc36913d44df27649c9de79eb3b5d.

---
+++ Theseus/app/control/ControlCategorie.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rpid6
+ * Date: 26/02/2015
+ * Time: 14:37
+ */
+
+namespace control;
+
+use model\Categorie;
+
+class ControlCategorie {
+
+    private $bdd;
+
+    public function __construct($bdd)
+    {
+        $this->bdd = $bdd;
+    }
+
+    public function getCategories(){
+        $req = $this->bdd->prepare('SELECT * FROM categorie');
+        $req->execute();
+        while($result = $req->fetch()){
+            $categorie = new Categorie($result);
+            $categories[] = $categorie;
+        }
+        return $categories;
+    }
+
+    public function getCategorie($id){
+        $req = $this->bdd->prepare('SELECT * FROM categorie WHERE id = :id');
+        $req->bindValue(':id',$id);
+        $req->execute();
+        if($result = $req->fetch()){
+            $categorie = new Categorie($result);
+        }
+        return $categorie;
+    }
+
+    public function addCategorie($categorie){
+        $req = $this->bdd->prepare('INSERT INTO categorie values (:nom,:image)');
+        $req->bindValue(':nom',$categorie->getNom());
+        $req->bindValue(':image',$categorie->getImage());
+    }
+}
---
+++ Theseus/app/model/Categorie.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rpid6
+ * Date: 27/02/2015
+ * Time: 11:52
+ */
+
+
+namespace model;
+
+
+class Categorie {
+
+    private $id;
+    private $nom;
+    private $image;
+
+    public function __construct($array){
+        $this->hydrate($array);
+    }
+
+    public function hydrate(array $array){
+        foreach ($array as $cle => $valeur) {
+            $method = 'set' . ucfirst($cle);
+            if (method_exists($this, $method)) {
+                $this->$method($valeur);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
