22f67dd69eaec1542b77aa905d6f7ff5240864ee
heisenberg7 <f.aymardruby@gmail.com>
2015-08-19 15:17:34 +0200

    Mon Compte
    - création page MyAccount.php

---
+++ Theseus/web/MyAccount.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 19/08/2015
+ * Time: 14:46
+ */
+
+include('template/header.php');
+?>
+<h2 class="header-title green nohover">Espace Mon Compte</h2>
+
+
+
+<?php
+include('template/footer.php');
