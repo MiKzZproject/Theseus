f0846002257008b9f601d914642b150b41ca960c
quaresma <quaresma217@gmail.com>
2015-08-21 01:36:59 +0200

    control qualité js

--- Theseus/web/js/script.js
+++ Theseus/web/js/script.js
@@ -1,5 +1,7 @@
 (function() {
 
+    "use strict";
+
     function searchCategorie(data) {
         $.ajax({
             method: "POST",
@@ -11,19 +13,19 @@
             });
     }
 
-    if(location.pathname == '/index.php' || location.pathname == '/') {
+    if(location.pathname === '/index.php' || location.pathname === '/') {
         $("#menuHome").addClass("navbar-brand");
         $(".navbar-title").append("Home");
-    } else if (location.pathname == '/events.php') {
+    } else if (location.pathname === '/events.php') {
         $("#menuEvent").addClass("navbar-brand");
         $(".navbar-title").append("Les Évènements");
-    } else if (location.pathname == '/products.php') {
+    } else if (location.pathname === '/products.php') {
         $("#menuProduct").addClass("navbar-brand");
         $(".navbar-title").append("Les Produits");
-    } else if (location.pathname == '/productsPhares.php') {
+    } else if (location.pathname === '/productsPhares.php') {
         $("#menuProductPhare").addClass("navbar-brand");
         $(".navbar-title").append("Les produits phares");
-    } else if (location.pathname == '/account.php') {
+    } else if (location.pathname === '/account.php') {
         $("#menuAccount").addClass("navbar-brand");
         $(".navbar-title").append("Mon compte");
     }
@@ -61,7 +63,6 @@
     $(".registerForm .btn-success").click(function registerClient(e) {
 
         var form = ($("#formRegister")).serialize();
-        console.log(form);
         $.ajax({
             method: "POST",
             url: "register.php",
@@ -79,37 +80,37 @@
             $("#formRegister")[0].reset();
         })
         .fail(function errorNewsletter(data) {
-            if (data.responseJSON['nom']) {
+            if (data.responseJSON.nom) {
                 $('#registerErrorNom').show();
             } else {
                 $('#registerErrorNom').hide();
             }
-            if (data.responseJSON['prenom']) {
+            if (data.responseJSON.prenom) {
                 $('#registerErrorPrenom').show();
             } else {
                 $('#registerErrorPrenom').hide();
             }
-            if (data.responseJSON['tel']) {
+            if (data.responseJSON.tel) {
                 $('#registerErrorTel').show();
             } else {
                 $('#registerErrorTel').hide();
             }
-            if (data.responseJSON['pwd']) {
+            if (data.responseJSON.pwd) {
                 $('#registerErrorPwd').show();
             } else {
                 $('#registerErrorPwd').hide();
             }
-            if (data.responseJSON['pwd2']) {
+            if (data.responseJSON.pwd2) {
                 $('#registerErrorPwd2').show();
             } else {
                 $('#registerErrorPwd2').hide();
             }
-            if (data.responseJSON['email']) {
+            if (data.responseJSON.email) {
                 $('#registerErrorEmail').show();
             } else {
                 $('#registerErrorEmail').hide();
             }
-            if (data.responseJSON['date']) {
+            if (data.responseJSON.date) {
                 $('#registerErrorDate').show();
             } else {
                 $('#registerErrorDate').hide();
@@ -119,7 +120,7 @@
     });
 
     $("#selectcatprod").change(function filterCategorie() {
-        searchCategorie($( "#selectcatprod option:selected").val())
+        searchCategorie($( "#selectcatprod option:selected").val());
     });
 
 })();
