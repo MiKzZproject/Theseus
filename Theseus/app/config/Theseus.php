d738a7caf09ae789b42ddbe9963b62818758f2a1
quaresma <quaresma217@gmail.com>
2015-09-03 23:20:24 +0200

    add file cong

---
+++ Theseus/app/config/Theseus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace config;
+
+class Theseus {
+    const DBDSN = 'mysql:host=localhost;dbname=theseus';
+    const DBUSER = 'root';
+    const DBMDP = '';
+    const NBPERPAGEPRODUCT = 9;
+}
+
