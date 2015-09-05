604d25284bb6ac078bdc98e7c41a7e6e5a00d82e
heisenberg7 <f.aymardruby@gmail.com>
2015-09-05 17:47:20 +0200

    Modif "Mon compte"
    - Ajout avantages prenium + css qui va avec

--- Theseus/web/MyAccount.php
+++ Theseus/web/MyAccount.php
@@ -19,7 +19,7 @@ include('template/header.php');
             <li role="presentation"><a href="#invitations" aria-controls="invitations" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-envelope"></span> Mes Invitations</a></li>
             <li role="presentation"><a href="#alertes" aria-controls="alertes" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-bell"></span> Mes alertes</a></li>
             <li role="presentation"><a href="#modifPassword" aria-controls="modifPassword" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-pencil"></span> Modification mot de passe</a></li>
-            <li role="presentation"><a href="#abo" aria-controls="abo" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-credit-card"></span> abonnement</a></li>
+            <li role="presentation"><a href="#abo" aria-controls="abo" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-credit-card"></span> Abonnement</a></li>
         </ul>
 
         <!-- Tabs -->
@@ -179,6 +179,41 @@ include('template/header.php');
             </div>
             <div role="tabpanel" class="tab-pane" id="abo">
                 <div class="container">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Avantages compte PRENIUM</h3>
+                        </div>
+                        <div class="panel panel-default bootcards-summary">
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-xs-6 col-sm-4 cardAbo">
+                                            <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+                                            <h4>Accès VIP = pas d'accès lors des évènements</h4>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-4 cardAbo">
+                                            <span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span>
+                                            <h4>Des exclusivités sur des produits</h4>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-4 cardAbo">
+                                            <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+                                            <h4>Accès aux évènements de votre choix = pas de tirage au sort</h4>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-4 cardAbo">
+                                            <span class="glyphicon glyphicon-plane" aria-hidden="true"></span>
+                                            <h4>Livraison gratuite à domicile des produits achetés (sous 24h)</h4>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-4 cardAbo">
+                                            <span class="glyphicon glyphicon-gift" aria-hidden="true"></span>
+                                            <h4>Des bons de réduction exclusifs à valoir sur nos prochaines ventes</h4>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-4 cardAbo" id="subscribe">
+                                            <span class=" glyphicon glyphicon-piggy-bank" aria-hidden="true"></span>
+                                            <h4>Inscription pour seulement 30€ !</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                         <input type="hidden" name="cmd" value="_s-xclick">
                         <input type="hidden" name="hosted_button_id" value="WQH6PRWJHK35J">
